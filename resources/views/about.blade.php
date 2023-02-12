b3812a342dca8bb9c01192f8112456f8bb727841
NeamotNaim <neamotnaim123@gmail.com>
2023-02-13 03:38:06 +0600

    about page added and welcome deleted

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-{{--     {{asset('')}}--}}
+    {{--     {{asset('')}}--}}
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href=" {{asset('css/bootstrap.min.css')}}"
     >
@@ -49,33 +49,49 @@
                         <a class="nav-link mr-4" href="{{route('home')}}">Home<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item mr-4">
-                        <a class="nav-link" href="{{route('about')}}">About</a>
+                        <a class="nav-link" href="#">About</a>
                     </li>
 
                 </ul>
 
 
-                    <a href="{{route('login')}}" class="btn btn-success my-2 my-sm-0 mr-3 p-3" >Login</a>
-                    <a href="{{route('register')}}" class="btn btn-success my-2 my-sm-0 p-3" >Registration</a>
+                <a href="{{route('login')}}" class="btn btn-success my-2 my-sm-0 mr-3 p-3" >Login</a>
+                <a href="{{route('register')}}" class="btn btn-success my-2 my-sm-0 p-3" >Registration</a>
 
             </div>
         </nav>
     </div>
 </div>
 <!-- banner section    -->
-<div class="banner">
+<div class="  ">
     <section class="text-gray-700 body-font">
         <div class="container px-5 py-24 mx-auto">
-            <div class="banner-body flex flex-col text-center w-full mb-12 ">
-                <h1 class="made_for sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Welcome to Course Distribution BSFMSTU</h1>
-                <p class="banner-sub lg:w-2/3 mx-auto leading-relaxed ">This site is only for distribute academic courses among CSE department teacher in Bangamata Sheikh
-                    Fojilatunnesa Mujib Science and Technology University</p>
-            </div>
-            <div class="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:px-0">
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>About Laravel</title>
+            </head>
+            <body>
 
-                <a href="{{route('register')}}"
-                    class=" banner-btn  m-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">get started now</a>
-            </div>
+
+           <b> <h1>Meet the Laravel Team</h1></b>
+            <br>
+            <ul class="ml-5">
+                <li>1. Anika Siamia Oyshi(19211101):project create,blade create, dashboard</li>
+                <br>
+                <li>2. Ashiquer Rahman(19111103): Home page design</li>
+                <br>
+                <li>3. Susmita Nandi(19211107):database create,routing,controller</li>
+                <br>
+                <li>4. Samia Nasir Nira(19211114):project login and registration system</li>
+                <br>
+                <li>5. A.K.M. Abdullah Al Masud(19111119):html,css static page creation</li>
+            </ul>
+            </body>
+            </html>
         </div>
     </section>
 
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
